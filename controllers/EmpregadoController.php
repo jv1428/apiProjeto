5531e928ff6e98310fd29a99ba32bcb1680f602b
Vieira <2160451@my.ipleiria.pt>
2018-01-02 16:56:38 +0000

    xpto quimquim

--- controllers/EmpregadoController.php
+++ controllers/EmpregadoController.php
@@ -9,9 +9,21 @@
 namespace app\controllers;
 
 
+use app\models\Empregado;
+use app\models\User;
 use yii\rest\ActiveController;
 
 class EmpregadoController extends ActiveController
 {
     public $modelClass = 'app\models\Empregado';
+
+    public function actionFiltro($tipo)
+    {
+        $dados = Empregado::find()
+            ->join('JOIN', 'user', 'user.id = empregado.id_user')
+            ->where('tipo_artigo.nome = :tipo', [':tipo' => $tipo])
+            ->all();
+
+        return $dados;
+    }
 }
